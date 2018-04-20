ab53499a19af33efa05e24190ea2ef570c714ec0
Dmitry Yastremskiy <Dmitry_Yastremskiy@epam.com>
2018-04-20 16:31:21 +0300

    updated config for prod and dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
-const path = require('path');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist'
-    },
-    module: {
-        rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-        ]
-    }
-};
+module.exports = (env, options) => ({
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devtool: options.mode === "development" ? "inline-source-map" : false,
+  devServer: {
+    contentBase: "./dist"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      }
+    ]
+  },
+  plugins: [new ExtractTextPlugin("style.css")]
+});
